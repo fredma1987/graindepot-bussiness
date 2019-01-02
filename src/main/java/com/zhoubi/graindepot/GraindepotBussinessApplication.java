8540e64486089d9d55ba5d616184305a6a50db43
fredma1987 <45970785+fredma1987@users.noreply.github.com>
2019-01-02 15:11:52 +0800

    初始化

---
+++ src/main/java/com/zhoubi/graindepot/GraindepotBussinessApplication.java
@@ -0,0 +1,14 @@
+package com.zhoubi.graindepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraindepotBussinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraindepotBussinessApplication.class, args);
+	}
+
+}
+
