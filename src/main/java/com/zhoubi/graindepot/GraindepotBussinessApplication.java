8c113c715842d10cb9e85f152d4e222ef70fd4f2
fredma1987 <356803326@qq.com>
2019-02-25 01:16:56 +0800

    调整-fred

--- src/main/java/com/zhoubi/graindepot/GraindepotBussinessApplication.java
+++ src/main/java/com/zhoubi/graindepot/GraindepotBussinessApplication.java
@@ -13,7 +13,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableEurekaClient
 @EnableFeignClients
 @MapperScan(value = "com.zhoubi.graindepot.mapper")
-@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
+@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE,maxInactiveIntervalInSeconds=4*60*60)
 public class GraindepotBussinessApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
